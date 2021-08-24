073cb147c17297c864ec036485e49d262eb08f0e
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-08-24 06:57:17 +0200

    first commit

--- Share-Button/ViewController.swift
+++ Share-Button/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func shareBtnPressed(_ sender: UIButton) {
+        let text = "Shared Content goes here"
+        let img = UIImage(named: "shuva")
+        let activityController = UIActivityViewController(activityItems: [text,img], applicationActivities: nil)
+        
+        activityController.excludedActivityTypes = [.airDrop, .postToFlickr]
+        self.present(activityController, animated: true)
+    }
+    
 }
 
